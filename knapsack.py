ae300efd7763b5216858c469f055d92036324caf
unknown <quyhoaphantruong@gmail.com>
2023-03-26 21:59:49 +0700

    branch

--- knapsack.py
+++ knapsack.py
@@ -2,29 +2,36 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 class Knapsack: 
     def __init__(self) -> None:
-        pass
+        self.capacity = self.input()["capacity"]
+        self.num_of_classes = self.input()["num_of_classes"]
+        self.weights = self.input()["weights"]
+        self.values = self.input()["values"]
+        self.labels = self.input()["labels"]
+
     
-    def input(): 
+    def input(self): 
         capacity = 0
         num_of_classes = 0
         weights = []
         values = []
         labels = []
-        
-        f = open("input.txt", "r")
-        print(f.read())
-        
-def input(): 
-    capacity = 0
-    num_of_classes = 0
-    weights = []
-    values = []
-    labels = []
-    
-    f = open("input.txt", "r")
-    lines = f.readlines()
-    print(lines)
 
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
+        f = open("input.txt", "r")
+        lines = f.readlines()
+        capacity = int(lines[0])
+        num_of_classes = int(lines[1])
+        weights = [int(i) for i in lines[2].split(',')]
+        values = [int(i) for i in lines[3].split(',')]
+        labels = [int(i) for i in lines[4].split(',')]
+        return {
+            "capacity": capacity,
+            "num_of_classes": num_of_classes,
+            "weights": weights, 
+            "values": values, 
+            "labels": labels
+        }
+
+        
+        
+a = Knapsack()
+print(a.capacity)
